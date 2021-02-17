f41999d0a5b46a1904eca53d42d09f646066ca8f
Steffen Mueller <maller@gmx.de>
2021-02-17 09:48:04 +0100

    fix homepage

--- pages/index.vue
+++ pages/index.vue
@@ -4,40 +4,52 @@
       <app-masthead/>
     </div>
     <div class="headings">
-          <h1 class="title">
-      smashing fuck
-    </h1>
-    <div v-for=""></div>
-    <p>Pleas eat</p>
+      <h1>
+        Hello tacoface
+        <br>I love delicious tacos
+      </h1>
+      <p>Please eat tacos.</p>
     </div>
-    <div class="links"><p>Got follow Maller</p></div>
-    <div class="nextpage">Projects</div>
-    
+    <div class="links">
+      <p>Go follow Hawk Philsworth</p>
+    </div>
+    <div class="nextpage">Projects ></div>
   </main>
 </template>
 
 <script>
-import AppMasthead from'~/components/AppMasthead.vue'
-
+import AppMasthead from "~/components/AppMasthead.vue";
 export default {
   components: {
     AppMasthead
   }
-}
+};
 </script>
 
-<style>
-main { 
+<style scoped>
+main {
   width: 100vw;
   height: calc(100vh - 100px);
-  display: grid; 
-  grid-template-columns: 100px repeat(4, 1fr); 
-  grid-template-rows: repeat(3, 1fr) 50px 100px; 
+  display: grid;
+  grid-template-columns: 100px 100px 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 50px 100px;
   grid-column-gap: 0px;
-  grid-row-gap: 0px; 
+  grid-row-gap: 0px;
 }
-.background { grid-area: 1 / 3 / 5 / 6; } 
-.headings { grid-area: 2 / 2 / 3 / 6; } 
-.links { grid-area: 5 / 1 / 6 / 5; padding: 30px;} 
-.nextpage { grid-area: 4 / 5 / 6 / 6; background: #eee; color: black; padding: 30px } 
+  .background {
+    grid-area: 1 / 3 / 5 / 6;
+  }
+  .headings {
+    grid-area: 2 / 2 / 3 / 6;
+  }
+  .links {
+    grid-area: 5 / 1 / 6 / 5;
+    padding: 30px;
+  }
+  .nextpage {
+    background: #eee;
+    color: black;
+    padding: 30px;
+    grid-area: 4 / 5 / 6 / 6;
+  }
 </style>
